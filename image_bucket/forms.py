3cd8049af9a6e09f15a60b02070ed4706abce789
Sagar Chand Agarwal <atomsagar@gmail.com>
2016-07-19 00:36:55 -0700

    models.py appended

---
+++ image_bucket/forms.py
@@ -0,0 +1,19 @@
+from flask.ext.wtf import Form, TextField, TextAreaField, SubmitField, validators, ValidationError, PasswordField
+class Signup(Form):
+  email = TextField("Email",  [validators.Required("Please enter your email address."), validators.Email("Please enter your email address.")])
+  password = PasswordField('Password', [validators.Required("Please enter a password.")])
+  submit = SubmitField("Create account")
+
+    def __init__(self, *args, **kwargs):
+    Form.__init__(self, *args, **kwargs)
+
+  def validate(self):
+    if not Form.validate(self):
+      return False
+
+    user = User.query.filter_by(email = self.email.data.lower()).first()
+    if user:
+      self.email.errors.append("That email is already taken")
+      return False
+    else:
+        return True
--- image_bucket/models.py
+++ image_bucket/models.py
@@ -1,3 +1,20 @@
 from flask.ext.sqlalchemy import SQLAlchemy
+from werkzeug import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
+
+class User(db.Model):
+  __tablename__ = 'users'
+  uid = db.Column(db.Integer, primary_key = True)
+  email = db.Column(db.String(12), unique=True)
+  password = db.Column(db.String(10))
+
+  def __init__(self, email, password):
+    self.email = email.lower()
+    self.set_password(password)
+
+  def set_password(self, password):
+    self.password = generate_password_hash(password)
+
+  def check_password(self, password):
+    return check_password_hash(self.password, password)
